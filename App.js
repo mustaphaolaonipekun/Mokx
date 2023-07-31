52831ce8f2f26831e1e82955484c9fed1a8ebb2c
mustydev <olaonipekunmustapha47@gmail.com>
2023-07-31 17:27:53 +0100

    added more javascript App

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-Get = (form) =>{
-    const InputValue = form.inputBox.value
-}
+const time = setInterval(() => {
+  const currentTime = new Date();
+  document.getElementById("time").textContent = currentTime.toLocaleTimeString(
+    [],
+    {
+      hour: "2-digit",
+      minute: "2-digit",
+    }
+  );
+}, 1000);
+
+Get = (form) => {
+  const InputValue = form.inputBox.value;
+};
 
 Get();
 
-newGet = () =>{
-    const NewValue = InputValue
-}
+newGet = () => {
+  const NewValue = InputValue;
+};
 
 newGet();
